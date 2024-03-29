f51d10d8a6e4f05316bb9b2b5c60554bc8ea1ddc
Charles <32884698+CharlesRK@users.noreply.github.com>
2024-03-28 22:49:24 -0400

    Update and rename droneMoveV1 to droneLand.py

--- drone/droneLand.py
+++ drone/droneLand.py
@@ -5,27 +5,27 @@ from olympe.messages.camera import start_video_streaming, stop_video_streaming
 
 def main():
     # Connect to the drone
-    with olympe.Drone("10.202.0.1") as drone:
+    with olympe.Drone("192.168.42.1) as drone:
         # Set the camera mode to video streaming
-        drone(set_camera_mode(cam_id=0, value="streaming"))
+        #drone(set_camera_mode(cam_id=0, value="streaming"))
 
         # Wait for the drone to be ready
-        drone.smart_sleep(5)
+        #drone.smart_sleep(5)
 
         # Take off
         drone(TakeOff()).wait()
 
         # Start video streaming
-        drone(start_video_streaming(cam_id=0))
+        #drone(start_video_streaming(cam_id=0))
 
-        try:
+        #try:
             # Fly straight for 3 seconds
-            drone(moveBy(0, 2, 0, 0)).wait()
-            drone.smart_sleep(3)
+            #drone(moveBy(0, 2, 0, 0)).wait()
+            #drone.smart_sleep(3)
 
-        finally:
+        #finally:
             # Stop video streaming
-            drone(stop_video_streaming(cam_id=0))
+            #drone(stop_video_streaming(cam_id=0))
 
             # Land
             drone(Landing()).wait()
